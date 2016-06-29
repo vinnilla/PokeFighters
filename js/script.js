256980f234f96eee25fb5e48fde5b6eb00660115
vinnilla <vincentckhuang@gmail.com>
2016-06-29 07:35:47 -0700

    implemented damage calculation on hit and created display for hp

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 //create players
 var p1 = {
+	name: 'Keydar',
 	health: 100,
 	damage: 10,
 	x: ['0px', '50px', '100px', '150px', '200px', '250px', '300px', '350px', '400px', '450px', '500px', '550px', '600px', '650px', '700px', '750px'],
-	counter: 0
+	counter: 0,
+	timerid: 0
 }
 
 var p2 = {
+	name: 'Dan',
 	health: 100,
 	damage: 10,
 	x: ['0px', '50px', '100px', '150px', '200px', '250px', '300px', '350px', '400px', '450px', '500px', '550px', '600px', '650px', '700px', '750px'],
-	counter: 15
+	counter: 15,
+	timerid: 0
 }
 
 //pointers to html elements
 p1HTML = document.getElementById('p1');
 p2HTML = document.getElementById('p2');
+p1HP = document.getElementById('p1hp');
+p2HP = document.getElementById('p2hp');
+
+//show starting hp
+updateHP();
 
 //movement
 document.addEventListener('keydown', function(e) {
@@ -35,32 +44,65 @@ document.addEventListener('keydown', function(e) {
 	else if (e.keyCode == 37) {
 		testMove(p2, 'left');
 	}
-	//collision detection
+}, false);
+
+//collision detection and hitting + damage calc
+document.addEventListener('keyup', function(e) {
+	//TODO change key layout for each palyer
 	// space
-	else if (e.keyCode == 32) {
+	if (e.keyCode == 32) {
 		//test collision
 		if (p1.counter+1 == p2.counter) {
-			collide(p2HTML);
+			//test hp of opponent
+			if (p2.health) {
+				p2.timerid = collide(p2HTML);
+				calcDamage(p1, p2);
+			}
 		}
 	}
+	//enter
 	else if (e.keyCode == 13) {
 		if (p2.counter-1 == p1.counter) {
-			collide(p1HTML);
+			if (p1.health) {
+				p1.timerid = collide(p1HTML);
+				calcDamage(p2, p1);
+			}
 		}
 	}
 }, false);
 
+function knockOut(aggressor, defender) {
+	if (!defender.health) {
+		console.log(defender.timerid)
+		clearInterval(defender.timerid);
+		//ensure flashing interval is cleared
+		setTimeout(function() {alert(aggressor.name + " KO'd " + defender.name);}, 500);
+	}
+}
+
+//TODO add blocking damage reduction
+function calcDamage(aggressor, defender){
+	defender.health -= aggressor.damage;
+	updateHP();
+	//KO
+	knockOut(aggressor, defender);
+}
+
+function updateHP() {
+	p1HP.innerHTML = p1.health;
+	p2HP.innerHTML = p2.health;
+}
+
 function collide(playerHit) {
-	var timer = 5;
-	var timerId = window.setInterval(function(){
-		console.log(timer);
-		playerHit.classList.toggle('image');
-		if (!timer) {
-			window.clearInterval(timerId);
-		}
-		timer --;
-	}, 250)
-		
+		var timer = 3;
+		var timerId = window.setInterval(function(){
+			playerHit.classList.toggle('image');
+			if (!timer) {
+				window.clearInterval(timerId);
+			}
+			timer --;
+		}, 250)
+		return timerId;
 };
 
 function testMove(player, direction) {
