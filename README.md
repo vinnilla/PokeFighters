b00136d6891c4136ea1a3f9adc50d21892ce0572
vinnilla <vincentckhuang@gmail.com>
2016-06-29 19:25:01 -0700

    added combo tracker and first stage evolution

--- README.md
+++ README.md
@@ -6,18 +6,23 @@ So far I only have 2 default boxes to check game mechanics. I have object collis
 Planned Features (some may not be implemented):
 
 Dan's Ideas: 
+
 -combo 3 - evolve to secondary form (squirtle -> wartortle)
+
 -combo 6 - evolve to final form (wartortle -> blastoise)
+
 -evolutions (increased stats) last for 5 seconds
 
 Combat:
--Attack cooldown
+
 -Separate attacks into high, normal and low -- probably implement crouching ability as well (attack while crouching to attack low and dodge high attacks at the same time)
+
   high attacks -- vulnerable spot so more damage
+
   low attacks -- faster, but less damage
 
 Movement:
+
 -Jumping(???)
+
 -dashing forward and backward to quickly engage/disengage
-
-
--- js/script.js
+++ js/script.js
@@ -3,29 +3,31 @@
 //create players
 var p1 = {
 	name: 'Keydar',
-	health: 100,
-	damage: 10,
 	x: ['0px', '50px', '100px', '150px', '200px', '250px', '300px', '350px', '400px', '450px', '500px', '550px', '600px', '650px', '700px', '750px'],
 	position: 0,
-	timerid: 0,
+	health: 100,
+	damage: 10,
+	attackCD: 0,
 	combo: 0,
+	comboId: 0,
+	timerid: 0,
 	blockEnergy: 20,
 	block: false,
-	attackCD: 0,
 	blockCD: 0
 }
 
 var p2 = {
 	name: 'Dan',
-	health: 100,
-	damage: 10,
 	x: ['0px', '50px', '100px', '150px', '200px', '250px', '300px', '350px', '400px', '450px', '500px', '550px', '600px', '650px', '700px', '750px'],
 	position: 15,
-	timerid: 0,
+	health: 100,
+	damage: 10,
+	attackCD: 0,
 	combo: 0,
+	comboId: 0,
+	timerid: 0,
 	blockEnergy: 20,
 	block: false,
-	attackCD: 0,
 	blockCD: 0
 }
 
@@ -34,11 +36,15 @@ p1HTML = document.getElementById('p1');
 p2HTML = document.getElementById('p2');
 p1HP = document.getElementById('p1hp');
 p2HP = document.getElementById('p2hp');
+var $p1Combo = $('#p1combo');
+var $p2Combo = $('#p2combo');
 
-//show starting hp
-updateHP();
+//show starting stats
+updateStats();
 document.addEventListener('keydown', movement, false);
 document.addEventListener('keyup', collisionDetection, false);
+
+// ------------------------------------------MOVEMENT------------------------------------------ //
 
 function movement(e) {
 	// d
@@ -58,127 +64,6 @@ function movement(e) {
 		testMove(p2, 'left');
 	}
 }
-
-function collisionDetection(e) {
-	//TODO change key layout for each player
-	//attack
-	// 1
-	if (e.keyCode == 49) {
-		//test collision
-		if (p1.position+1 == p2.position) {
-			//test hp of opponent and attack cooldown
-			if (p2.health && p1.attackCD <= 0) {
-				p2.timerid = collide(p2HTML);
-				calcDamage(p1, p2);
-			}
-		}
-	}
-	// .
-	else if (e.keyCode == 190) {
-		if (p2.position-1 == p1.position) {
-			if (p1.health && p2.attackCD <= 0) {
-				p1.timerid = collide(p1HTML);
-				calcDamage(p2, p1);
-			}
-		}
-	}
-
-	//block
-	// 2
-	else if (e.keyCode == 50) {
-		block(p1, p1HTML);
-	}
-	// /
-	else if (e.keyCode == 191) {
-		block(p2, p2HTML);	
-	}
-}
-
-function block(player, html) {
-	if (player.blockCD <= 0) {
-		//3 second cd
-		player.blockCD = 3;
-		player.block = true;
-		toggleBlock(html);
-		//make block false after 1 second
-		setTimeout(function() {
-			player.block = false;
-			toggleBlock(html);
-		}, 1000);
-		console.log(player.block);
-		//countdown block cool down
-		var id = setInterval(function() {
-			player.blockCD--;
-			if (player.blockCD <= 0) {
-				clearInterval(id);
-			}
-		}, 1000);
-	}
-}
-
-function toggleBlock(player) {
-	player.classList.toggle('image');
-	player.classList.toggle('block');
-}
-
-function resetBlock (player) {
-	player.block = false;
-}
-
-function knockOut(aggressor, defender) {
-	if (defender.health <= 0) {
-		console.log(defender.timerid)
-		clearInterval(defender.timerid);
-		//remove event listeners
-		document.removeEventListener('keydown', movement);
-		document.removeEventListener('keyup', collisionDetection);
-		//ensure flashing interval is cleared
-		setTimeout(function() {alert(aggressor.name + " KO'd " + defender.name);}, 500);
-	}
-}
-
-//TODO add blocking damage reduction
-function calcDamage(aggressor, defender){
-	//check block
-	if (!defender.block) {
-		defender.health -= aggressor.damage;
-	}
-	else { //damage halved when block
-		defender.health -= aggressor.damage/2;
-	}
-	updateHP();
-	setAttackCD(aggressor);
-	//KO
-	knockOut(aggressor, defender);
-}
-
-function setAttackCD(aggressor, move) {
-	aggressor.attackCD = 1;
-	//reset attackCD to 0
-	var id = setInterval(function() {
-		aggressor.attackCD --;
-		if (aggressor.attackCD <=0) {
-			clearInterval(id);
-		}
-	}, 500);
-}
-
-function updateHP() {
-	p1HP.innerHTML = p1.health;
-	p2HP.innerHTML = p2.health;
-}
-
-function collide(playerHit) {
-		var timer = 3;
-		var timerId = window.setInterval(function(){
-			playerHit.classList.toggle('image');
-			if (!timer) {
-				window.clearInterval(timerId);
-			}
-			timer --;
-		}, 250)
-		return timerId;
-};
 
 function testMove(player, direction) {
 	//p1 moving right
@@ -212,4 +97,156 @@ function testMove(player, direction) {
 			p2HTML.style.left = p2.x[p2.position];
 		} 
 	}
+}
+
+// ------------------------------------------COLLISION AND COMBAT------------------------------------------ //
+
+function collisionDetection(e) {
+	//TODO change key layout for each player
+	//attack
+	// 1
+	if (e.keyCode == 49) {
+		//test collision
+		if (p1.position+1 == p2.position) {
+			//test hp of opponent and attack cooldown
+			if (p2.health && p1.attackCD <= 0) {
+				p2.timerid = collide(p2HTML);
+				calcDamage(p1, p2, p1HTML, p2HTML);
+			}
+		}
+	}
+	// .
+	else if (e.keyCode == 190) {
+		if (p2.position-1 == p1.position) {
+			if (p1.health && p2.attackCD <= 0) {
+				p1.timerid = collide(p1HTML);
+				calcDamage(p2, p1, p2HTML, p1HTML);
+			}
+		}
+	}
+
+	//block
+	// 2
+	else if (e.keyCode == 50) {
+		block(p1, p1HTML);
+	}
+	// /
+	else if (e.keyCode == 191) {
+		block(p2, p2HTML);	
+	}
+}
+
+function collide(playerHit) {
+		var timer = 3;
+		var timerId = window.setInterval(function(){
+			playerHit.classList.toggle('standard');
+			if (!timer) {
+				window.clearInterval(timerId);
+			}
+			timer --;
+		}, 250)
+		return timerId;
+};
+
+// ------------------------------------------ATTACKING------------------------------------------ //
+
+//TODO add blocking damage reduction
+function calcDamage(aggressor, defender, aggressorHTML, defenderHTML){
+	//check block
+	if (!defender.block) {
+		defender.health -= aggressor.damage;
+	}
+	else { //damage halved when block
+		defender.health -= aggressor.damage/2;
+	}
+	setAttackCD(aggressor);
+	//combo tracking
+	aggressor.combo++;
+	clearInterval(aggressor.comboId);
+	aggressor.comboId = comboReset(aggressor);
+	evolution(aggressor, aggressorHTML);
+	updateStats();
+	//KO
+	knockOut(aggressor, defender);
+}
+
+function setAttackCD(aggressor, move) {
+	aggressor.attackCD = 1;
+	//reset attackCD to 0
+	var id = setInterval(function() {
+		aggressor.attackCD --;
+		if (aggressor.attackCD <=0) {
+			clearInterval(id);
+		}
+	}, 500);
+}
+
+// ------------------------------------------COMBO MANAGEMENT------------------------------------------ //
+
+function comboReset(player) {
+	//reset combo after 5 seconds
+	var id = setTimeout(function(){
+		player.combo = 0;
+		updateStats();
+	},5000);
+	return id;
+}
+
+function evolution(player, html) {
+	if (player.combo >=3) {
+		html.classList.remove('standard');
+		html.classList.add('evo1');
+	}
+}
+
+// ------------------------------------------BLOCKING------------------------------------------ //
+
+function block(player, html) {
+	if (player.blockCD <= 0) {
+		//3 second cd
+		player.blockCD = 3;
+		player.block = true;
+		toggleBlock(html);
+		//make block false after 1 second
+		setTimeout(function() {
+			player.block = false;
+			toggleBlock(html);
+		}, 1000);
+		console.log(player.block);
+		//countdown block cool down
+		var id = setInterval(function() {
+			player.blockCD--;
+			if (player.blockCD <= 0) {
+				clearInterval(id);
+			}
+		}, 1000);
+	}
+}
+
+function toggleBlock(player) {
+	player.classList.toggle('standard');
+	player.classList.toggle('block');
+}
+
+// ------------------------------------------ROUND END------------------------------------------ //
+
+function knockOut(aggressor, defender) {
+	if (defender.health <= 0) {
+		console.log(defender.timerid)
+		clearInterval(defender.timerid);
+		//remove event listeners
+		document.removeEventListener('keydown', movement);
+		document.removeEventListener('keyup', collisionDetection);
+		//ensure flashing interval is cleared
+		setTimeout(function() {alert(aggressor.name + " KO'd " + defender.name);}, 500);
+	}
+}
+
+function updateStats() {
+	//hp
+	p1HP.innerHTML = p1.health;
+	p2HP.innerHTML = p2.health;
+	//combo
+	$p1Combo.text(p1.combo);
+	$p2Combo.text(p2.combo);
 }
