9101be03bb00a76fa808866e197022e3dcfd6590
vinnilla <vincentckhuang@gmail.com>
2016-06-30 18:36:25 -0700

    added round end reset functionality and incorporated absolute centering

--- js/script.js
+++ js/script.js
@@ -4,17 +4,23 @@ var p1 = {
 	// x: ['0px', '50px', '100px', '150px', '200px', '250px', '300px', '350px', '400px', '450px', '500px', '550px', '600px', '650px', '700px', '750px', '800px', '850px', '900px', '950px', '1000px', '1064px'],
 	x: ['0px', '50px', '100px', '150px', '200px', '250px', '300px', '350px', '400px', '450px', '500px', '550px', '600px', '650px', '700px', '750px', '800px', '850px', '900px'],
 	position: 0,
+	nPosition: 0,
 	health: 100,
+	nHealth: 100,
 	damage: 10,
-	attackCD: 0,
+	nDamage: 10,
 	attackSpeed: 10,
+	nAttackSpeed: 10,
+	attackCD: 0,
 	stun: false,
 	combo: 0,
 	comboId: 0,
 	timerid: 0,
 	blockStrength: 2,
+	nBlockStrength: 2,
 	block: false,
-	blockCD: 0
+	blockCD: 0,
+	wins: 0
 }
 
 var p2 = {
@@ -23,15 +29,20 @@ var p2 = {
 	x: ['0px', '50px', '100px', '150px', '200px', '250px', '300px', '350px', '400px', '450px', '500px', '550px', '600px', '650px', '700px', '750px', '800px', '850px', '900px'],
 	//position: 21,
 	position: 18,
+	nPosition: 18,
 	health: 100,
+	nHealth: 100,
 	damage: 10,
-	attackCD: 0,
+	nDamage: 10,
 	attackSpeed: 10,
+	nAttackSpeed: 10,
+	attackCD: 0,
 	stun:false,
 	combo: 0,
 	comboId: 0,
 	timerid: 0,
 	blockStrength: 2,
+	nBlockStrength: 2,
 	block: false,
 	blockCD: 0
 }
@@ -51,7 +62,8 @@ var $timer = $('#timer');
 //show starting stats
 updateStats();
 //start round timer
-startTimer();
+var timer = 180;
+var timerID = startTimer();
 
 $document.on('keydown', movement);
 $document.on('keyup', collisionDetection);
@@ -268,7 +280,6 @@ function toggleBlock(player) {
 // ------------------------------------------ROUND END------------------------------------------ //
 
 function startTimer() {
-	var timer = 180;
 	$timer.text(timer);
 	var id = setInterval(function() {
 		timer--;
@@ -277,6 +288,52 @@ function startTimer() {
 			clearInterval(id);
 		}
 	}, 1000);
+	return id;
+}
+
+function updateStats() {
+	//hp
+	p1HP.text(p1.health);
+	p2HP.text(p2.health);
+	//combo
+	$p1Combo.text(p1.combo);
+	$p2Combo.text(p2.combo);
+}
+
+function resetGame(p1, p2, p1html, p2html) {
+	//reset position
+	p1.position = p1.nPosition;
+	p2.position = p2.nPosition;
+	//reset health
+	p1.health = p1.nHealth;
+	p2.health = p2.nHealth;
+	//reset combat mechanisms
+	p1.attackCD = p2.attackCD = 0;
+	p1.stun = p2.stun = false;
+	p1.combo = p2.combo = 0;
+	p1.block = p2.block = false;
+	p1.blockCD = p2.blockCD = 0;
+	//reset stat changes due to evolution
+	p1.attackSpeed = p1.nAttackSpeed;
+	p2.attackSpeed = p2.nAttackSpeed;
+	p1.damage = p1.nDamage;
+	p2.damage = p2.nDamage;
+	p1.blockStrength = p1.nBlockStrength;
+	p2.blockStrength = p2.nBlockStrength;
+	p1html.removeClass('evo1').addClass('standard');
+	p2html.removeClass('evo1').addClass('standard');
+	//reset timer
+	clearInterval(timerID);
+	//start timer and show stats
+	timer = 180;
+	timerID = startTimer();
+	updateStats();
+	//shift models back
+	move(p1, p1HTML, 0)
+	move(p2, p2HTML, 0)
+
+	$document.on('keydown', movement);
+	$document.on('keyup', collisionDetection);
 }
 
 function knockOut(aggressor, defender) {
@@ -287,15 +344,7 @@ function knockOut(aggressor, defender) {
 		$document.off('keydown', movement);
 		$document.off('keyup', collisionDetection);
 		//ensure flashing interval is cleared
-		setTimeout(function() {alert(aggressor.name + " KO'd " + defender.name);}, 500);
+		setTimeout(function() {alert(aggressor.name + " KO'd " + defender.name + '\n New Round starting in 3 seconds.');}, 500);
+		resetGame(p1, p2, p1HTML, p2HTML);
 	}
-}
-
-function updateStats() {
-	//hp
-	p1HP.text(p1.health);
-	p2HP.text(p2.health);
-	//combo
-	$p1Combo.text(p1.combo);
-	$p2Combo.text(p2.combo);
 }
